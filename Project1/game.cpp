6c014729840d6d9c25a3bf754852bbef7580f589
IlSanna <federico.sannasardo@outlook.com>
2019-10-26 21:39:52 +0200

    Added input class
    also limited fps

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -1,6 +1,15 @@
 #include <SDL.h>
 #include "game.h"
 #include "graphics.h"
+#include "input.h"
+#include <algorithm>
+
+namespace {
+	const int FPS = 50;
+	//maximum amount of time a frame is allowed to last
+	const int MAX_FRAME_TIME = 5 * 1000 / FPS;
+}
+
 
 Game::Game() {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -12,14 +21,39 @@ Game::~Game() {
 
 void Game::gameLoop() {
 	Graphics graphics;
+	Input input;
 	SDL_Event event;
 
+	int LAST_UPDATE_TIME = SDL_GetTicks();
+
 	while (true) {
+
+		input.beginNewFrame();
+
 		if (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+			if (event.type == SDL_KEYDOWN) {
+				if (event.key.repeat == 0) {//make sure your are not holding down the key
+					input.keyDownEvent(event);
+				}
+			}
+			else if (event.type == SDL_KEYUP) {
+				input.keyUpEvent(event);
+			}
+			else if (event.type == SDL_QUIT) {
 				return;
 			}
 		}
+		if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE)) {
+			return;
+		}
+		const int CURRENT_TIME_MS = SDL_GetTicks();
+		//take the time of the last loop and subtracting it
+		//from current time i get elapsed time
+		int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
+		//i have to pass to update the elapsed time and this value need to be
+		//bounded between elapsed time and max frame time
+		update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
+		LAST_UPDATE_TIME = CURRENT_TIME_MS; 
 	}
 }
 
