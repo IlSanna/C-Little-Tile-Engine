4c1aa3b068e8d63d1571f00b93771c7af7567586
IlSanna <federico.sannasardo@outlook.com>
2019-11-01 15:41:48 +0100

    added tile class def and decl

---
+++ Project1/headers/Tile.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SDL.h>
+#include "graphics.h"
+#include "globals.h"
+
+class Tile {
+public:
+	Tile();
+	Tile(SDL_Texture* tileset, Vector2 size, Vector2 tilesetPosition, Vector2 position);
+	void update(float elapsedTime);
+	void draw(Graphics &graphics);
+private:
+	SDL_Texture* _tileset;
+	Vector2 _size;
+	Vector2 _tilesetPosition;//position on the tileset
+	Vector2 _position;//position on the map
+};
---
+++ Project1/src/tile.cpp
@@ -0,0 +1,33 @@
+#include "../headers/Tile.h"
+
+Tile::Tile() {
+}
+
+Tile::Tile(SDL_Texture * tileset, Vector2 size, Vector2 tilesetPosition, Vector2 position) :
+	_tileset(tileset),
+	_size(size),
+	_tilesetPosition(tilesetPosition),
+	_position(Vector2(position.x * globals::SPRITE_SCALE, position.y * globals::SPRITE_SCALE) )
+{
+
+}
+
+void Tile::update(float elapsedTime) {
+
+}
+
+void Tile::draw(Graphics & graphics) {
+	SDL_Rect destRect = {
+		_position.x,
+		_position.y,
+		_size.x * globals::SPRITE_SCALE,
+		_size.y * globals::SPRITE_SCALE
+	};
+	SDL_Rect sourceRect = {
+		_position.x,
+		_position.y,
+		_size.x ,
+		_size.y
+	};
+	graphics.blitSurface(_tileset, &sourceRect, &destRect);
+}
